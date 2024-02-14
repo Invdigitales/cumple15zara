00e7751a8931914402aebd86f3b23d086659024a
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-02-14 09:07:13 -0300

    1

---
+++ main.js
@@ -0,0 +1,79 @@
+
+let diaEvento = new Date('March 15, 2024 18:30:00').getTime();
+let boton = document.querySelector("#btn")
+
+
+
+// diaEvento = new Date().getTime() + 1_000 * 20;
+
+let relojE = document.getElementById('reloj');
+
+const diaM = 86_400_000;
+const horaM = 3_600_000;
+const minutoM = 60_000;
+const segundoM = 1_000;
+const ejecutarReloj = setInterval(
+    () => {
+
+        let hoy = new Date().getTime();
+        let distancia = diaEvento - hoy;
+
+        let diaR = Math.trunc(distancia / diaM);
+
+        let horaR = Math.trunc((distancia % diaM) / horaM);
+
+        let minutoR = Math.trunc((distancia % horaM) / minutoM);
+
+        let segundoR = Math.trunc((distancia % minutoM) / segundoM);
+
+        if (distancia  < 1) {
+            clearInterval(ejecutarReloj);
+            diaEventoAhora();
+        }
+
+        pintarElementos(diaR, horaR, minutoR, segundoR);
+
+    }
+, 1000);
+
+let creDia = document.getElementById('cre-dia');
+let creHora = document.getElementById('cre-hora');
+let creMinuto = document.getElementById('cre-minuto');
+let creSegundo = document.getElementById('cre-segundo');
+function pintarElementos(dia, hora, minuto, segundo) {
+
+    segundo = (segundo < 10) ? '0' + segundo : segundo;
+
+    minuto = (minuto < 10) ? '0' + minuto : minuto;
+
+    hora = (hora < 10) ? '0' + hora : hora;
+
+    creDia.innerHTML = dia;
+    creHora.innerHTML = hora;
+    creMinuto.innerHTML = minuto;
+    creSegundo.innerHTML = segundo;
+}
+
+function diaEventoAhora () {
+    alert('Dia del Evento!!!');
+    // Todo el codigo cuando el evento llegue
+}
+
+
+
+
+
+
+  boton.addEventListener("click", () => {
+
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<if8335!28!4f13.1!3m3!1m2!1s0x9432a348c5c382cb%3A0xaafc13ca9f3043ed!2sBloom%20Fiestas%20Infantiles!5e0!3m2!1ses-419!2sar!4v1707851628165!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        'Podes ampliar el mapa',
+        
+        
+    })
+  });
